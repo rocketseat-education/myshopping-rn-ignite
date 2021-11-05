e6385a1d9adc86e7c10a35809b369179e00747fd
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-05 20:00:00 -0300

    instalacao e configuracao emulador firestore

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import 'react-native-gesture-handler';
+
+import '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
+
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+
+if (__DEV__) {
+  firestore().useEmulator('192.168.0.100', 8080);
+}
+
+const db = firestore();
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
