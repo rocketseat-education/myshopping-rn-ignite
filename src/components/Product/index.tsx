116f986b5f343329c8ebf7c191d199fb839ab3d5
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-05 19:34:25 -0300

    atualizando um documento

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import firestore from '@react-native-firebase/firestore';
 
 import { ButtonIcon } from '../ButtonIcon';
 import { Container, Info, Title, Quantity, Options } from './styles';
@@ -15,6 +16,16 @@ type Props = {
 }
 
 export function Product({ data }: Props) {
+
+  function handleDoneToggle() {
+    firestore()
+      .collection('products')
+      .doc(data.id)
+      .update({
+        done: !data.done
+      });
+  }
+
   return (
     <Container>
       <Info>
@@ -30,6 +41,7 @@ export function Product({ data }: Props) {
       <Options>
         <ButtonIcon
           icon={data.done ? "undo" : "check"}
+          onPress={handleDoneToggle}
         />
 
         <ButtonIcon
--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -11,9 +11,6 @@ export function ShoppingList() {
   useEffect(() => {
     const subscribe = firestore()
       .collection('products')
-      .orderBy('quantity')
-      .startAt(3)
-      .endAt(5)
       .onSnapshot(querySnapshot => {
         const data = querySnapshot.docs.map((doc) => {
           return {
