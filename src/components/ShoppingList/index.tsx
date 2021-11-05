210cc74f01465cf16d1abf6d4543b288a4ee7403
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-05 18:59:17 -0300

    limitando a consulta

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -11,7 +11,7 @@ export function ShoppingList() {
   useEffect(() => {
     const subscribe = firestore()
       .collection('products')
-      .where('quantity', '==', 1)
+      .limit(3)
       .onSnapshot(querySnapshot => {
         const data = querySnapshot.docs.map((doc) => {
           return {
