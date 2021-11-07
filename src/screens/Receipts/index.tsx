2cd66b8770d0818bc4ec2a22ea876c2f4ac0f7c0
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-07 11:59:41 -0300

    removendo arquivos

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, Alert } from 'react-native';
 import storage from '@react-native-firebase/storage';
 
 import { Container, PhotoInfo } from './styles';
@@ -20,7 +20,18 @@ export function Receipts() {
     setPhotoInfo(`Upload realizado em ${info.timeCreated}`);
   }
 
-  useEffect(() => {
+  async function handleDeleteImage(path: string) {
+    storage()
+      .ref(path)
+      .delete()
+      .then(() => {
+        Alert.alert('Imagem excluída com sucesso!');
+        fetchImages();
+      })
+      .catch(error => console.error(error));
+  }
+
+  async function fetchImages() {
     storage().ref('images').list().then(result => {
       const files: FileProps[] = [];
 
@@ -33,6 +44,10 @@ export function Receipts() {
 
       setPhotos(files);
     });
+  }
+
+  useEffect(() => {
+    fetchImages();
   }, []);
 
   return (
@@ -52,7 +67,7 @@ export function Receipts() {
           <File
             data={item}
             onShow={() => handleShowImage(item.path)}
-            onDelete={() => { }}
+            onDelete={() => handleDeleteImage(item.path)}
           />
         )}
         contentContainerStyle={{ paddingBottom: 100 }}
