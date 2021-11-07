42027ada0b394f796957ecd563630f9d9f4838bc
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-07 11:48:45 -0300

    obtendo url da imagem para exibir

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -9,6 +9,12 @@ import { File, FileProps } from '../../components/File';
 
 export function Receipts() {
   const [photos, setPhotos] = useState<FileProps[]>([]);
+  const [photoSelected, setPhotoSelected] = useState('');
+
+  async function handleShowImage(path: string) {
+    const urlImage = await storage().ref(path).getDownloadURL();
+    setPhotoSelected(urlImage);
+  }
 
   useEffect(() => {
     storage().ref('images').list().then(result => {
@@ -25,12 +31,11 @@ export function Receipts() {
     });
   }, []);
 
-
   return (
     <Container>
       <Header title="Comprovantes" />
 
-      <Photo uri="" />
+      <Photo uri={photoSelected} />
 
       <PhotoInfo>
         Informações da foto
@@ -42,7 +47,7 @@ export function Receipts() {
         renderItem={({ item }) => (
           <File
             data={item}
-            onShow={() => { }}
+            onShow={() => handleShowImage(item.path)}
             onDelete={() => { }}
           />
         )}
