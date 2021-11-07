ed109a592c5098a326d196831ebef04b769ad6eb
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-07 10:57:17 -0300

    progresso do upload

--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -11,6 +11,8 @@ import { Container, Content, Progress, Transferred } from './styles';
 
 export function Upload() {
   const [image, setImage] = useState('');
+  const [bytesTransferred, setBytesTransferred] = useState('');
+  const [progress, setProgress] = useState('0');
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -32,10 +34,18 @@ export function Upload() {
     const fileName = new Date().getTime();
     const reference = storage().ref(`/images/${fileName}.png`);
 
-    reference
-      .putFile(image)
-      .then(() => Alert.alert('Upload concluído!'))
-      .catch((error) => console.error(error));
+    const uploadTask = reference.putFile(image);
+
+    uploadTask.on('state_changed', taskSnapshot => {
+      const percent = ((taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) * 100).toFixed(0);
+      setProgress(percent);
+
+      setBytesTransferred(`${taskSnapshot.bytesTransferred} transferido de ${taskSnapshot.totalBytes}`);
+    });
+
+    uploadTask.then(() => {
+      Alert.alert('Upload concluído com sucesso!')
+    });
   }
 
   return (
@@ -51,11 +61,11 @@ export function Upload() {
         />
 
         <Progress>
-          0%
+          {progress}%
         </Progress>
 
         <Transferred>
-          0 de 100 bytes transferido
+          {bytesTransferred}
         </Transferred>
       </Content>
     </Container>
