309a1263c14e348c04289d460745f8d92d5cd7a7
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-11 17:18:55 -0300

    Observando se o usuário está logado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
 
 import { AppRoutes } from './app.routes';
 import { SignIn } from '../screens/SignIn';
 
+type User = {
+  uid: string;
+}
+
 export function Routes() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
   return (
     <NavigationContainer>
       <SignIn />
