bdf0b690b65c7e9ee6b1059e1cdb05bab54da82f
rodrigo <rodrigo.rgtic@gmail.com>
2021-11-11 17:52:23 -0300

    Resetar a senha com e-mail

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -51,6 +51,12 @@ export function SignIn() {
       });
   }
 
+  function handleForgotPassword() {
+    auth()
+      .sendPasswordResetEmail(email)
+      .then(() => Alert.alert('Enviamos um link no seu e-mail para você redefinir sua senha.'));
+  }
+
   return (
     <Container>
       <Title>MyShopping</Title>
@@ -71,7 +77,7 @@ export function SignIn() {
       <Button title="Entrar" onPress={handleSignInWithEmailAndPassword} />
 
       <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
+        <ButtonText title="Recuperar senha" onPress={handleForgotPassword} />
         <ButtonText title="Criar minha conta" onPress={handleCreateUserAccount} />
       </Account>
     </Container>
