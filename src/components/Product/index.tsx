269043043c473ccf7ba949af5120afb63d0c05e8
Rodrigo Lima <rodrigofflima@hotmail.com>
2022-01-17 14:27:55 -0300

    feat: config firestore offline and edit/delete products

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import firestore from '@react-native-firebase/firestore';
 
 import { ButtonIcon } from '../ButtonIcon';
 import { Container, Info, Title, Quantity, Options } from './styles';
@@ -8,34 +9,39 @@ export type ProductProps = {
   description: string;
   quantity: number;
   done: boolean;
-}
+};
 
 type Props = {
   data: ProductProps;
-}
+};
 
 export function Product({ data }: Props) {
+  function handleDoneToggle() {
+    firestore().collection('products').doc(data.id).update({
+      done: !data.done,
+      description: 'Coca-Cola'
+    });
+  }
+
+  function handleDelete() {
+    firestore().collection('products').doc(data.id).delete();
+  }
+
   return (
     <Container>
       <Info>
-        <Title done={data.done}>
-          {data.description}
-        </Title>
+        <Title done={data.done}>{data.description}</Title>
 
-        <Quantity>
-          Quantidade: {data.quantity}
-        </Quantity>
+        <Quantity>Quantidade: {data.quantity}</Quantity>
       </Info>
 
       <Options>
         <ButtonIcon
-          icon={data.done ? "undo" : "check"}
+          icon={data.done ? 'undo' : 'check'}
+          onPress={handleDoneToggle}
         />
 
-        <ButtonIcon
-          icon="delete"
-          color="alert"
-        />
+        <ButtonIcon icon='delete' color='alert' onPress={handleDelete} />
       </Options>
     </Container>
   );
--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -9,19 +9,19 @@ export function ShoppingList() {
   const [products, setProducts] = useState<ProductProps[]>([]);
 
   useEffect(() => {
-    firestore()
+    const subscribe = firestore()
       .collection('products')
-      .get()
-      .then(response => {
-        const data = response.docs.map(doc => {
+      .onSnapshot(querySnapshot => {
+        const data = querySnapshot.docs.map(doc => {
           return {
             id: doc.id,
             ...doc.data()
           };
         }) as ProductProps[];
         setProducts(data);
-      })
-      .catch(error => console.log(error));
+      });
+
+    return () => subscribe();
   }, []);
 
   return (
