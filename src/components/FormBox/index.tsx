11aa2e0fcd8361c01a22891eb79d05d61e038149
Rodrigo Lima <rodrigofflima@hotmail.com>
2022-01-16 22:02:01 -0300

    feat: add products to firebase

--- src/components/FormBox/index.tsx
+++ src/components/FormBox/index.tsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 import { Container } from './styles';
 import { ButtonIcon } from '../ButtonIcon';
 import { Input } from '../Input';
 
 export function FormBox() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(0);
+
+  async function handleProductAdd() {
+    firestore()
+      .collection('products')
+      .add({
+        description,
+        quantity,
+        done: false
+      })
+      .then(() => {
+        Alert.alert('Produto adicionado com sucesso');
+      })
+      .catch(error => console.log(error));
+  }
+
   return (
     <Container>
       <Input
-        placeholder="Nome do produto"
-        size="medium"
+        placeholder='Nome do produto'
+        size='medium'
+        onChangeText={setDescription}
       />
 
       <Input
-        placeholder="0"
-        keyboardType="numeric"
-        size="small"
+        placeholder='0'
+        keyboardType='numeric'
+        size='small'
         style={{ marginHorizontal: 8 }}
+        onChangeText={value => setQuantity(Number(value))}
       />
 
       <ButtonIcon
         size='large'
-        icon="add-shopping-cart"
-        onPress={() => { }}
+        icon='add-shopping-cart'
+        onPress={handleProductAdd}
       />
     </Container>
   );
